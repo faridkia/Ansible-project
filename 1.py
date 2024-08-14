e1061878c0c76a3ec68e968007191f9f85d8f383
faridkia <farid.1382.k@gmail.com>
2024-08-14 16:34:48 +0330

    fix final

--- 1.py
+++ 1.py
@@ -1,10 +1,5 @@
 import time
-import sys
 
-log_file_path = '/var/log/my_python_app.log'
-
-sys.stdout = open(log_file_path, 'a')
-sys.stderr = open(log_file_path, 'a')
 
 while True:
     print("I'm alive")
