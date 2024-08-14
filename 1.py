20369e11943431122153a79d218f9a83570b5e86
faridkia <farid.1382.k@gmail.com>
2024-08-14 16:17:38 +0330

    fix

--- 1.py
+++ 1.py
@@ -1,5 +1,11 @@
 import time
- 
+import sys
+
+log_file_path = '/var/log/my_python_app.log'
+
+sys.stdout = open(log_file_path, 'a')
+sys.stderr = open(log_file_path, 'a')
+
 while True:
     print("I'm alive")
     time.sleep(5)
